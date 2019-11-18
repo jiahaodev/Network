1f0d539f1bb88ea9f8072c5fb7fe7333f5a65425
JiahaoWu <jiahaowu@boyaa.com>
2019-11-18 17:02:09 +0800

    更新程序入口

--- server/Server/Program.cs
+++ server/Server/Program.cs
@@ -8,9 +8,16 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Server");
-        Console.WriteLine("[服务器]启动完成");
-        new Final().Execute();  //最终版本
+        //Console.WriteLine("Hello Server");
+        //Console.WriteLine("[服务器]启动完成");
+        //new Final().Execute();  //最终版本
+
+        //最终版本
+        if (DbManager.Connect("game", "127.0.0.1", 3306, "root", ""))
+        {
+            return;
+        }
+        NetManager.StartLoop(8888);
     }
 }
 
