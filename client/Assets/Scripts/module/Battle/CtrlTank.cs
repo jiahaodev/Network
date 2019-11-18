3e6c8a782e29e98f53c22e50bdda45365089d6f3
JiahaoWu <jiahaowu@boyaa.com>
2019-11-18 20:20:55 +0800

    添加玩家控制的坦克类

---
+++ client/Assets/Scripts/module/Battle/CtrlTank.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlTank : BaseTank {
+	//上一次发送同步信息的时间
+
+	//同步频率
+
+	//自带瞄准的坦克
+
+	//更新
+
+
+
+	//移动控制
+
+	//炮塔控制
+
+	//开炮
+
+
+}
