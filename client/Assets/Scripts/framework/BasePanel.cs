8c02f4a77d5c31ac10dfad3c2e75ef21a8449a4a
JiahaoWu <jiahaowu@boyaa.com>
2019-11-18 19:15:27 +0800

    添加类定义

---
+++ client/Assets/Scripts/framework/BasePanel.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class BasePanel : MonoBehaviour{
+	//皮肤路径
+	public string skinPath;
+	//皮肤
+	public GameObject skin;
+	//层级
+	public PanelManager.Layer layer = PanelManager.Layer.Panel;
+	
+	//初始化
+	public void Init(){
+		GameObject skinPrefab = ResManager.LoadPrefab(skinPath);
+		skin = (GameObject)Instantiate(skinPrefab);
+	}
+	//关闭
+	public void Close(){
+		string name = this.GetType().ToString();
+		PanelManager.Close(name);
+	}
+	
+
+	//初始化时
+	public virtual void OnInt(){
+
+	}
+
+	//显示时
+	public virtual void OnShow(params object[] para){
+
+	}
+
+	//关闭时
+	public virtual void OnClose(){
+
+	}
+
+}
