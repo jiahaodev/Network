9e713531a55e18b12ede3a6461642ed802a187d3
JiahaoWu <jiahaodev@163.com>
2019-11-20 02:20:52 +0800

    更新双端工程

--- server/Server/Program.cs
+++ server/Server/Program.cs
@@ -6,18 +6,29 @@ using System.Threading.Tasks;
 
 public class Program
 {
+
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Server");
         //Console.WriteLine("[服务器]启动完成");
         //new Final().Execute();  //最终版本
 
+        Test();
+
+        /*
+
         //最终版本
-        if (DbManager.Connect("game", "127.0.0.1", 3306, "root", ""))
+        if (!DbManager.Connect("tankgame", "127.0.0.1", 3306, "root", ""))
         {
             return;
         }
         NetManager.StartLoop(8888);
+        */
+    }
+
+    static void Test() {
+        new TestDB().execute();
+
     }
 }
 
